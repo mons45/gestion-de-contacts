485a549fe37fdb62114a794922ccc30768e1b180
monse45 <monsefsaadouni03@gmail.com>
2024-12-27 18:15:25 +0100

    add all files

---
+++ src/routes/destroy.jsx
@@ -0,0 +1,13 @@
+import { redirect } from "react-router-dom";
+import { deleteContact } from "../contacts";
+
+export async function action({ params }) {
+  try {
+    await deleteContact(params.contactId);
+    return redirect("/");
+  } catch (error) {
+    console.error("Failed to delete contact:", error);
+    
+    return redirect("/error"); 
+  }
+}
